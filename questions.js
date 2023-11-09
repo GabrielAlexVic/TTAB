a51923f3673b133288b95cb4eaf27df232a525f1
gabriel.victorio <gabriel.victorio@7comm.com.br>
2023-11-08 22:50:59 -0300

    Invert sim e nao

--- questions.js
+++ questions.js
@@ -8,31 +8,31 @@ export default [
       "exemplo, telefonou para amigos no meio da noite? " +
       "(Avaliar sobre autoconfiança e sociabilidade).",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "2- Você estava tão irritado que gritava com as pessoas ou " +
     "começava brigas ou discussões?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "3- Você se sentiu muito mais autoconfiante do que o normal?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "4- Você dormiu muito menos do que o normal e descobriu que " +
     "realmente não sentia falta?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
 
@@ -40,51 +40,51 @@ export default [
   {
     question: "5- Você estava muito mais falante ou falava mais rápido do que o normal?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "6- Os pensamentos passaram pela sua cabeça ou você não conseguiu desacelerar sua mente?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "7- Você se distraiu tão facilmente com as coisas ao seu redor " +
     "que teve problemas para se concentrar ou manter o foco?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "8- Você teve muito mais energia do que o normal?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "9- Você foi muito mais ativo ou fez muito mais coisas do que o normal?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "10- Você estava muito mais interessado em sexo do que o normal?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
   {
     question: "11- Gastar dinheiro colocou você ou sua familia em apuros?",
     answers: [
-      { option: "Não", correct: false },
       { option: "Sim", correct: true },
+      { option: "Não", correct: false },
     ],
   },
 
@@ -93,23 +93,23 @@ export default [
       question: "12- Houve ao menos 6 períodos diferentes de tempo (pelo menos " +
       "2 semanas) em que você se sentiu profundamente deprimido?",
       answers: [
-        { option: "Não", correct: false },
         { option: "Sim", correct: true },
+        { option: "Não", correct: false },
       ],
     },
     {
       question: "13- Você teve problemas com depressão antes dos 18 anos?",
       answers: [
-        { option: "Não", correct: false },
         { option: "Sim", correct: true },
+        { option: "Não", correct: false },
       ],
     },
     {
       question: "14- Você já teve que parar ou trocar seu antidepressivo porque o " +
       "deixou altamente irritado ou hiperativo?",
       answers: [
-        { option: "Não", correct: false },
         { option: "Sim", correct: true },
+        { option: "Não", correct: false },
       ],
     },
 ];
